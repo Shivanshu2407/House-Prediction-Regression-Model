52f8bbf0855603dd12e726f870a1bcc3d7586f05
Shivanshu Tiwari <st955218@gmail.com>
2025-04-02 01:18:14 +0530

    web ui

--- Client/app.js
+++ Client/app.js
@@ -1,59 +1,58 @@
 function getBathValue() {
-  var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for(var i in uiBathrooms) {
-    if(uiBathrooms[i].checked) {
-        return parseInt(i)+1;
-    }
-  }
-  return -1; // Invalid Value
+  let bathrooms = document.querySelector('input[name="uiBathrooms"]:checked');
+  return bathrooms ? parseInt(bathrooms.value) : -1;
 }
 
 function getBHKValue() {
-  var uiBHK = document.getElementsByName("uiBHK");
-  for(var i in uiBHK) {
-    if(uiBHK[i].checked) {
-        return parseInt(i)+1;
-    }
-  }
-  return -1; // Invalid Value
+  let bhk = document.querySelector('input[name="uiBHK"]:checked');
+  return bhk ? parseInt(bhk.value) : -1;
 }
 
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked");
-  var sqft = document.getElementById("uiSqft");
-  var bhk = getBHKValue();
-  var bathrooms = getBathValue();
-  var location = document.getElementById("uiLocations");
-  var estPrice = document.getElementById("uiEstimatedPrice");
+  
+  let sqft = document.getElementById("uiSqft").value;
+  let bhk = getBHKValue();
+  let bathrooms = getBathValue();
+  let location = document.getElementById("uiLocations").value;
+  let estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "http://127.0.0.1:5000/predict_home_price";
+  if (!sqft || bhk === -1 || bathrooms === -1 || !location) {
+      estPrice.innerHTML = "<h2>Please enter all details</h2>";
+      return;
+  }
+
+  let url = "http://127.0.0.1:5000/predict_home_price";
 
   $.post(url, {
-      total_sqft: parseFloat(sqft.value),
+      total_sqft: parseFloat(sqft),
       bhk: bhk,
       bath: bathrooms,
-      location: location.value
-  },function(data, status) {
-      console.log(data.estimated_price);
-      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-      console.log(status);
+      location: location
+  }, function(data, status) {
+      console.log("Response received:", data);
+      estPrice.innerHTML = `<h2>${data.estimated_price} Lakh</h2>`;
+  }).fail(function() {
+      estPrice.innerHTML = "<h2>Error fetching price</h2>";
   });
 }
 
 function onPageLoad() {
-  console.log("document loaded");
-  var url = "http://127.0.0.1:5000/get_location_names";
-  $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
-      if(data) {
-          var locations = data.locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
-          }
+  console.log("Document loaded");
+  let url = "http://127.0.0.1:5000/get_location_names";
+  
+  $.get(url, function(data, status) {
+      console.log("Received location data");
+      if (data && data.locations) {
+          let uiLocations = document.getElementById("uiLocations");
+          uiLocations.innerHTML = '<option value="" disabled selected>Choose a Location</option>';
+          data.locations.forEach(location => {
+              let opt = new Option(location, location);
+              uiLocations.add(opt);
+          });
       }
+  }).fail(function() {
+      console.log("Failed to load locations");
   });
 }
 
